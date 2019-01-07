ba6fa23e9c4060406297d5bfbceda794e61b74ae
Brian Grunkemeyer <brian@flexcharging.com>
2019-01-06 18:20:39 -0800

    Print out more interesting information about vehicles.

--- TeslaConsole/Program.cs
+++ TeslaConsole/Program.cs
@@ -21,11 +21,33 @@ namespace TeslaConsole
 
             foreach (TeslaVehicle car in vehicles)
             {
-                Console.WriteLine(car.Id + " " + car.Vin);
-                var climateState = car.LoadClimateStateStatus();
-                Console.WriteLine($"Climate State Inside: {climateState.InsideTemperature}  Outside: {climateState.OutsideTemperature}");
+                Console.WriteLine(car.DisplayName + "   VIN: " + car.Vin);
+                Console.Write("Waking up...  ");
+                car.WakeUp();
+                Console.WriteLine("Done");
+
+                var vehicleState = car.LoadVehicleStateStatus();
+                if (vehicleState == null)
+                    Console.WriteLine("Vehicle state was null!  Is the car not awake?");
+                else
+                    Console.WriteLine(" Roof state: {0}", vehicleState.PanoramicRoofState);
+
+                var chargeState = car.LoadChargeStateStatus();
+                Console.WriteLine($" Desired State of charge: {chargeState.ChargeLimitSoc}%");
+                car.LoadDriveStateStatus();
+                car.LoadClimateStateStatus();
+                car.LoadGuiStateStatus();
+                car.LoadMobileEnabledStatus();
+                var options = car.Options;
+                Console.WriteLine($"  Battery size: {options.BatterySize}  Has firmware limit? {(options.BatteryFirmwareLimit.HasValue ? options.BatteryFirmwareLimit.ToString() : false.ToString())}");
+                Console.WriteLine($"  Charger limit: {options.ChargerLimit}");
+                Console.WriteLine("  Odometer: {0}", car.LoadVehicleStateStatus().Odometer);
+                Console.WriteLine("Climate:");
+                var climate = car.LoadClimateStateStatus();
+                Console.WriteLine($"  Driver temperature: {climate.DriverTemperatureSetting}  Passenger: {climate.PassengerTemperatureSetting}");
             }
 
+            Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
     }
