d4fbb7368d54ad482ce2ba37b36de11a2e9bfc4c
Rohit Chandel <rohit@flexcharging.com>
2022-07-27 05:33:37 +0000

    TeslaService changes.

---
+++ TeslaLib/ServiceNotAvailableException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeslaLib
+{
+    public class ServiceNotAvailableException : Exception
+    {
+        public ServiceNotAvailableException() : base("Tesla's service is unavailable")
+        {
+        }
+
+        public ServiceNotAvailableException(String message) : base(message)
+        {
+        }
+    }
+}
