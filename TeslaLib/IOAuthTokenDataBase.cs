29e44a56c55aabc728dde51b2320d2ad7dbcd104
Jitendra Rajbhar <jitendra@flexcharging.com>
2023-08-01 17:12:36 +0530

    Added a segregated Interface(IOAuthTokenDataBase.cs) in TeslaLib to add/updated OAuthToken in DB using Tesla and Flexcharging emailaddress

---
+++ TeslaLib/IOAuthTokenDataBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using TeslaLib.Models;
+
+namespace TeslaLib
+{
+    public interface IOAuthTokenDataBase : IOAuthTokenStore
+    {
+        /// <summary>
+        /// Add Tesla OAuth token into DB
+        /// </summary>
+        /// <param name="flexEmailAddress">Flexcharging email address</param>
+        /// <param name="teslaEmailAddress">Tesla account email address</param>
+        /// <param name="token">Tesla OAuth token</param> 
+        Task AddOAuthTokenAsync(string flexEmailAddress, string teslaEmailAddress, LoginToken token);
+
+        /// <summary>
+        /// Update Tesla OAuth token into DB
+        /// </summary>
+        /// <param name="flexEmailAddress">Flexcharging email address</param>
+        /// <param name="teslaEmailAddress">Tesla account email address</param>
+        /// <param name="token">Tesla OAuth token</param> 
+        Task UpdateOAuthTokenAsync(string flexEmailAddress, string teslaEmailAddress, LoginToken token);
+    }
+}
